f45b4da0ffbe5f61271c395df3c3497f152e4d76
ZhangWentao <Lazziixxx@gmail.com>
2021-03-16 10:04:29 +0800

    Update main.go

--- base/main.go
+++ base/main.go
@@ -1,10 +1,12 @@
 package main
 
-type Engine struct{}
-
 /* 基于Engine实现了ServeHTTP方法 */
 /* 利用ResponseWriter指针可以构造对该请求的响应 */
 /* 第一步：将所有请求转向了自己的处理逻辑 */
+
+/*
+type Engine struct{}
+
 func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
   switch req.URL.Path {
     case "/":
@@ -21,4 +23,20 @@ func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 func main() {
   engine := new(Engine)
   log.Fatal(http.ListenAndServe(":9999", engine))
+}
+*/
+
+func main() {
+  r := gee.New()
+  r.GET("/", func(w http.ResponseWriter, req *http.Request) {
+    fmt.Fprintf(w, "URL.Path=%q\n", req.URL.Path)
+  })
+  
+  r.GET("/Hello", func(w http.ResponseWriter, req *http.Request) {
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+  })
+  
+  r.RUN(":9999")
 }
