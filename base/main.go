922c5053df99f00db7e579859bcb9a8b83a58171
ZhangWentao <Lazziixxx@gmail.com>
2021-03-16 10:10:44 +0800

    Update main.go

--- base/main.go
+++ base/main.go
@@ -1,8 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"gee"
+)
+
 /* 基于Engine实现了ServeHTTP方法 */
 /* 利用ResponseWriter指针可以构造对该请求的响应 */
-/* 第一步：将所有请求转向了自己的处理逻辑 */
+/* ---->将所有请求转向了自己的处理逻辑 */
 
 /*
 type Engine struct{}
@@ -27,16 +34,16 @@ func main() {
 */
 
 func main() {
-  r := gee.New()
-  r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "URL.Path=%q\n", req.URL.Path)
-  })
-  
-  r.GET("/Hello", func(w http.ResponseWriter, req *http.Request) {
+	r := gee.New()
+	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "URL.Path=%q\n", req.URL.Path)
+	})
+
+	r.GET("/HelloRex", func(w http.ResponseWriter, req *http.Request) {
 		for k, v := range req.Header {
 			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
 		}
-  })
-  
-  r.RUN(":9999")
+	})
+
+	r.RUN(":9999")
 }
